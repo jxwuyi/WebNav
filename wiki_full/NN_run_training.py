3bf846ff54e28550bbbdb6c1f7f52f72ba10f460
jxwuyi <jxwuyi@gmail.com>
2016-04-26 22:15:29 -0700

    add a test code

--- wiki_full/NN_run_training.py
+++ wiki_full/NN_run_training.py
@@ -3,6 +3,7 @@ import vin_webnav as vn
 import vin_webnav_fast as vnf
 import vin_webnav_baseline as bsl
 import vin_webnav_combine as cmb
+import vin_webnav_combine_test as cmbt
 import myparameters as prm
 
 
@@ -16,7 +17,7 @@ def main():
     parser.add_argument("--dropout", action="store_true")
     parser.add_argument("--stepsize", type=float, default=.0002)
     parser.add_argument("--model",
-                        choices=["dense1", "dense2", "dense3", "conv", "WikiBaseLine", "WikiCombine", "valIterWiki", "valIterWebNav","valIterWebNavFast", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
+                        choices=["dense1", "dense2", "dense3", "conv", "WikiBaseLine", "WikiCombine", "WikiCombineTest","valIterWiki", "valIterWebNav","valIterWebNavFast", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
                         default="valIterWebNav")
     parser.add_argument("--unittest", action="store_true")
     parser.add_argument("--grad_check", action="store_true")
@@ -52,6 +53,12 @@ def main():
                     seed = args.seed)
     elif (args.model == "WikiCombine"):
         my_nn = cmb.vin_web(model=args.model, N = prm.total_pages, D=prm.max_links_per_page,
+                    emb_dim = prm.dim_emb, dropout=args.dropout,
+                    devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
+                    k=args.k, seed = args.seed)
+        my_nn.load_pretrained()
+    elif (args.model == "WikiCombineTest"):
+        my_nn = cmbt.vin_web(model=args.model, N = prm.total_pages,
                     emb_dim = prm.dim_emb, dropout=args.dropout,
                     devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
                     k=args.k, seed = args.seed)
