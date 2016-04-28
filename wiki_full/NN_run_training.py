f8590b49a70bb5db068482b74a2e10e11d19973b
jxwuyi <jxwuyi@gmail.com>
2016-04-27 19:21:20 -0700

    add simplified model

--- wiki_full/NN_run_training.py
+++ wiki_full/NN_run_training.py
@@ -17,7 +17,7 @@ def main():
     parser.add_argument("--dropout", action="store_true")
     parser.add_argument("--stepsize", type=float, default=.0002)
     parser.add_argument("--model",
-                        choices=["dense1", "dense2", "dense3", "conv", "WikiBaseLine", "WikiCombine", "WikiCombineTest","valIterWiki", "valIterWebNav","valIterWebNavFast", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
+                        choices=["dense1", "dense2", "dense3", "conv", "WikiBaseLine", "WikiProj", "WikiCombine", "WikiCombineTest","valIterWiki", "valIterWebNav","valIterWebNavFast", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
                         default="valIterWebNav")
     parser.add_argument("--unittest", action="store_true")
     parser.add_argument("--grad_check", action="store_true")
@@ -58,6 +58,12 @@ def main():
                     k=args.k, seed = args.seed)
         my_nn.load_pretrained()
     elif (args.model == "WikiCombineTest"):
+        my_nn = cmbt.vin_web(model=args.model, N = prm.total_pages,
+                    emb_dim = prm.dim_emb, dropout=args.dropout,
+                    devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
+                    k=args.k, seed = args.seed, batchsize = args.batchsize)
+        my_nn.load_pretrained()
+    elif (args.model == "WikiProj"):
         my_nn = cmbt.vin_web(model=args.model, N = prm.total_pages,
                     emb_dim = prm.dim_emb, dropout=args.dropout,
                     devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
