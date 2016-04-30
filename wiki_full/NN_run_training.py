6065ef07b033a2aa5ea1d0d7c9b32f1a11902f44
jxwuyi <jxwuyi@gmail.com>
2016-04-29 20:01:43 -0700

    add new model

--- wiki_full/NN_run_training.py
+++ wiki_full/NN_run_training.py
@@ -7,6 +7,7 @@ import vin_webnav_combine_test as cmbt
 import vin_webnav_proj as prj
 import vin_webnav_proj_sg as prj_sg
 import vin_webnav_proj_sanity as prj_san
+import vin_webnav_proj_sanity_bias as prj_san_b
 import vin_webnav_proj_sanity2 as prj_san2
 import vin_webnav_proj_sanity3 as prj_san3
 import vin_webnav_proj_sanity_full as prj_sanF
@@ -25,7 +26,7 @@ def main():
     parser.add_argument("--dropout", action="store_true")
     parser.add_argument("--stepsize", type=float, default=.0002)
     parser.add_argument("--model",
-                        choices=["dense1", "dense2", "dense3", "conv", "WikiBaseLine", "WikiProj","WikiProjAtt","WikiProjAtt2","WikiProjSanityFull","WikiProjSanity","WikiProjSanity3","WikiProjSanity2","WikiProjSG", "WikiCombine", "WikiCombineTest","valIterWiki", "valIterWebNav","valIterWebNavFast", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
+                        choices=["dense1", "dense2", "dense3", "conv", "WikiBaseLine", "WikiProj","WikiProjAtt","WikiProjAtt2","WikiProjSanityFull","WikiProjSanity","WikiProjSanityBias","WikiProjSanity3","WikiProjSanity2","WikiProjSG", "WikiCombine", "WikiCombineTest","valIterWiki", "valIterWebNav","valIterWebNavFast", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
                         default="valIterWebNav")
     parser.add_argument("--unittest", action="store_true")
     parser.add_argument("--grad_check", action="store_true")
@@ -85,6 +86,11 @@ def main():
         my_nn.load_pretrained()
     elif (args.model == "WikiProjSanity"):
         my_nn = prj_san.vin_web(model=args.model, N = prm.total_pages,
+                    emb_dim = prm.dim_emb, dropout=args.dropout,
+                    devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
+                    seed = args.seed, batchsize = args.batchsize)
+    elif (args.model == "WikiProjSanityBias"):
+        my_nn = prj_san_b.vin_web(model=args.model, N = prm.total_pages,
                     emb_dim = prm.dim_emb, dropout=args.dropout,
                     devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
                     seed = args.seed, batchsize = args.batchsize)
