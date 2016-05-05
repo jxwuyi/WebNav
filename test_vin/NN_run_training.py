dd19efc021eb90d6959b80f12327e9ad5ca6be49
jxwuyi <jxwuyi@gmail.com>
2016-05-04 21:59:27 -0700

    add baseline mode for wikiSchool

--- test_vin/NN_run_training.py
+++ test_vin/NN_run_training.py
@@ -1,6 +1,7 @@
 from NNobj import *
 import vin_wiki_sanity as vn_san
 import vin_wiki_fast as vn
+import vin_baseline as bsl
 import myparameters as prm
 
 
@@ -14,7 +15,7 @@ def main():
     parser.add_argument("--dropout", action="store_true")
     parser.add_argument("--stepsize", type=float, default=.0002)
     parser.add_argument("--model",
-                        choices=["dense1", "dense2", "dense3", "conv", "valIterWiki", "valIterMultiBatch", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
+                        choices=["dense1", "dense2", "dense3", "conv","WikiBaseline", "valIterWiki", "valIterMultiBatch", "valIterBatch", "CBvalIterBatch", "valIterMars", "valIterMarsSingle"],
                         default="valIterWiki")
     parser.add_argument("--unittest", action="store_true")
     parser.add_argument("--grad_check", action="store_true")
@@ -28,6 +29,8 @@ def main():
     parser.add_argument("--maxhops", type=int, default=1)
     parser.add_argument("--stepdecreaserate", type=float, default=1.0)
     parser.add_argument("--stepdecreasetime", type=int, default=10000)
+    parser.add_argument("--reportgap", type=int, default=10000)
+    parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--sanity_check", default="None")
     args = parser.parse_args()
 
@@ -38,23 +41,34 @@ def main():
                     devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
                     k=args.k, A=args.A,
                     batchsize=args.batchsize, maxhops=args.maxhops)
-    else:
+    elif (args.model == "valIterWiki"):
         # VI network
         my_nn = vn.vin(model=args.model, N = prm.total_pages, D=prm.max_links_per_page,
                     emb_dim = prm.dim_emb, dropout=args.dropout,
                     devtype=args.devtype, grad_check=args.grad_check, reg=args.reg,
                     k=args.k, A=args.A,
                     batchsize=args.batchsize, maxhops=args.maxhops)
+    elif (args.model == "WikiBaseline"):
+        # Baseline model
+        my_nn = bsl.vin(model=args.model, emb_dim=prm.dim_emb, devtype=args.devtype,
+                        batchsize=args.batchsize, seed=args.seed,
+                        report_gap = args.reportgap)
         
         
     if args.warmstart != "None":
         print('warmstarting...')
         my_nn.load_weights(args.warmstart)
-    if args.sanity_check != "None":
-        my_nn.run_training_sanity_check(stepsize=args.stepsize,  epochs=args.epochs)
-    else:
-        my_nn.run_training(stepsize=args.stepsize, epochs=args.epochs,
+
+    try:
+        if args.sanity_check != "None":
+            my_nn.run_training_sanity_check(stepsize=args.stepsize,  epochs=args.epochs)
+        else:
+            my_nn.run_training(stepsize=args.stepsize, epochs=args.epochs,
                            grad_check=args.grad_check)
+
+    except KeyboardInterrupt:
+        print "Training interupted!!!"
+    
     my_nn.save_weights(outfile=str(args.output))
 
 if __name__ == "__main__":
